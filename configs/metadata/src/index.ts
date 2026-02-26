4278cd145dcc7467d12a1d1102cf4f467b5b147f
Xu <hxhxhx88@gmail.com>
2026-02-26 07:43:01 -0800

    fix: update bundle ID to com.xu.dishcover.app

--- configs/metadata/src/index.ts
+++ configs/metadata/src/index.ts
@@ -3,4 +3,4 @@ export const APP_NAME = 'DishCover'
 // Android package name does not allow dash(-), while iOS bundle identifier does not allow underscore(_).
 // So make sure app_slug only contains alphanumeric characters.
 export const APP_SLUG = 'dishcover'
-export const APP_PACKAGE_NAME = `com.${APP_SLUG}.app`
+export const APP_PACKAGE_NAME = `com.xu.${APP_SLUG}.app`
